86fde2e6d1e028a43dcca75c31059e1e2eeae54c
MaksimKavalenka <maksim.kavalenka@gmail.com>
2019-03-04 01:42:28 +0300

    Task 3: Implementation of the 'list' function

--- task3/main.go
+++ task3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -17,13 +18,18 @@ const (
 	itemArg      = "item"
 	fileNameArg  = "fileName"
 	operationArg = "operation"
+
+	addOps      = "add"
+	findByIdOps = "findById"
+	removeOps   = "remove"
+	listOps     = "list"
 )
 
 var operationArguments = map[string][]string{
-	"add":      {itemArg, fileNameArg},
-	"findById": {idArg, fileNameArg},
-	"remove":   {idArg, fileNameArg},
-	"list":     {fileNameArg},
+	addOps:      {itemArg, fileNameArg},
+	findByIdOps: {idArg, fileNameArg},
+	removeOps:   {idArg, fileNameArg},
+	listOps:     {fileNameArg},
 }
 
 func (error *MyError) Error() string {
@@ -57,8 +63,44 @@ func parseArgs() Arguments {
 	return nil
 }
 
+func add(item, fileName string) {
+
+}
+
+func findById(id, fileName string) string {
+	return ""
+}
+
+func remove(id, fileName string) {
+
+}
+
+func list(fileName string) []byte {
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return content
+}
+
 func Perform(args Arguments, writer io.Writer) error {
-	return checkArguments(args)
+	err := checkArguments(args)
+	if err != nil {
+		return err
+	}
+
+	switch args[operationArg] {
+	case addOps:
+		add(args[itemArg], args[fileNameArg])
+	case findByIdOps:
+		findById(args[idArg], args[fileNameArg])
+	case removeOps:
+		remove(args[idArg], args[fileNameArg])
+	case listOps:
+		writer.Write(list(args[fileNameArg]))
+	}
+
+	return nil
 }
 
 func main() {
